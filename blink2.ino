2a726337a0e5d00b72aa97147d0d87f5963f0d70
20b01a0406 <103505953+20b01a0406@users.noreply.github.com>
2022-04-22 22:52:01 +0530

    Add files via upload

---
+++ blink2.ino
@@ -0,0 +1,36 @@
+int led1=8;
+int led2=9;
+int led3=10;
+int led4=11;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+  delay(100);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+  delay(100);   
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,LOW);
+  delay(100);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,HIGH);
+  delay(100);
+
+}
