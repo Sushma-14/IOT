d5f3103d40358ebbf757e5078687162253c1177e
20b01a0406 <103505953+20b01a0406@users.noreply.github.com>
2022-04-22 22:52:51 +0530

    Add files via upload

---
+++ fading.ino
@@ -0,0 +1,18 @@
+int led=11;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ analogWrite(11,512);
+ delay(100);
+ analogWrite(11,256);
+ delay(100);
+ analogWrite(11,128);
+ delay(200);
+ analogWrite(11,0);
+ delay(200);
+}
